eabe4583bc7fc640a1b05ee1609196de72726a01
VinhDevNguyen <vinh2000ld14@gmail.com>
2020-04-17 01:38:41 +0700

    Find Neighbor

--- LAB01/Exercise/Ex4_Matrix.py
+++ LAB01/Exercise/Ex4_Matrix.py
@@ -1,5 +1,7 @@
+# %%
 # Read data
-data = open("LAB01/Exercise/Maps/map1.txt")
+import numpy as np
+data = open("Maps/map1.txt")
 m, n = data.readline().split()
 m = int(m)
 n = int(n)
@@ -10,22 +12,54 @@ Map_Matrix = []
 for i in range(n):
     Map_Matrix.append(data.readline().rstrip('\n').split(' '))
     Map_Matrix[i] = [int(e) for e in Map_Matrix[i]]
-print(Map_Matrix)
-data.seek(0)
-wholeData = data.readlines()
-print(wholeData)
+Map_Matrix = np.reshape(Map_Matrix, (-1,n))
+
+# %% [markdown]
+# Locate D1 and D2
+for i in range(m):
+    for e in range(n):
+        if Map_Matrix[i][e] == 1:
+            D1.append(i)
+            D1.append(e)
+        if Map_Matrix[i][e] == 2:
+            D2.append(i)
+            D2.append(e)
 
 
+# %% [markdown]
+# Find Neigbor
+Neigh_List = []
+try:
+    Map_Matrix[D1[0]+1, D1[1]]
+except IndexError as e:
+    print("Ngoai matrix")
+else:
+    print(D1[0]+1, D1[1])
+    Neigh_List.append([D1[0]+1, D1[1]])
+# %%
+try:
+    Map_Matrix[D1[0]-1, D1[1]]
+except IndexError as e:
+    print("Ngoai matrix")
+else:
+    print(D1[0]-1, D1[1])
+    Neigh_List.append([D1[0]-1, D1[1]])
+# %%
+try:
+    Map_Matrix[D1[0], D1[1]+1]
+except IndexError as e:
+    print("Ngoai matrix")
+else:
+    print(D1[0], D1[1]+1)
+    Neigh_List.append([D1[0], D1[1]+1])
 
+# %%
+try:
+    Map_Matrix[D1[0], D1[1]-1]
+except IndexError as e:
+    print("Ngoai matrix")
+else:
+    print(D1[0], D1[1]-1)
+    Neigh_List.append([D1[0], D1[1]-1])
 
-# # Locate D1 and D2
-# for i in range(m):
-#     for e in range(n):
-#         if Map_Matrix[i][e] == 1:
-#             D1.append(i)
-#             D1.append(e)
-#         if Map_Matrix[i][e] == 2:
-#             D2.append(i)
-#             D2.append(e)
-# print(D1)
-# print(D2)
+# %%
