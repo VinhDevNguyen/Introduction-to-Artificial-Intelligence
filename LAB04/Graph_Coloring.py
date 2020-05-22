1d881687a9daadec9ea80d509987c5e8ed33d871
VinhDevNguyen <vinh2000ld14@gmail.com>
2020-05-22 15:50:35 +0700

    Fix Graph_coloring

--- LAB04/Graph_Coloring.py
+++ LAB04/Graph_Coloring.py
@@ -25,9 +25,9 @@ for node in graph:
             #     node_color = node_color.replace(node_color.split("-",1)[1],str(colors[1]))
             # elif(node_color.split("-",1)[1] == Neighbor.split("-",1)[1] == colors[1]):
             #     node_color = node_color.replace(node_color.split("-",1)[1],str(colors[2]))
-            if(visited[Neighbor] == colors[0]):
+            if(visited[Neighbor] == visited[node] == colors[0]):
                 visited[node] = colors[1]
-            if(visited[Neighbor] == colors[1]):
+            elif(visited[Neighbor] == visited[node] == colors[1]):
                 visited[node] = colors[2]
 
 
